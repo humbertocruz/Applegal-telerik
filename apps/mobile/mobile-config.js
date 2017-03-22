ce18f2ce9e1e5135a6fcba282de8117e515183b4
Humberto Cruz <humberto.cruz@gmail.com>
2017-03-22 20:44:24 -0300

    logo

--- apps/mobile/mobile-config.js
+++ apps/mobile/mobile-config.js
@@ -1,24 +1,24 @@
 App.info({
-	id: 'br.com.applegal.com.br',
-	name: 'Techno App',
-	description: "App Tehcnotronics",
+	id: 'br.com.technotronics.igreja',
+	name: 'EscoladasNacoes',
+	description: "Escola das Nações",
 	author: 'Humberto Cruz',
 	email: 'humberto.cruz@gmail.com',
 	website: 'https://applegal.com.br',
-	version: '0.0.2'
+	version: '0.1.0'
 });
 App.icons({
-	'android_mdpi': '../../src-techno/Android/res/drawable-mdpi/icon.png',
-	'android_hdpi': '../../src-techno/Android/res/drawable-hdpi/icon.png',
-	'android_xhdpi': '../../src-techno/Android/res/drawable-xhdpi/icon.png',
-	'android_xxhdpi': '../../src-techno/Android/res/drawable-xxhdpi/icon.png',
-	'android_xxxhdpi': '../../src-techno/Android/res/drawable-xxxhdpi/icon.png'
+	'android_mdpi': '../../../src-nacoes/Android/res/drawable-mdpi/icon.png',
+	'android_hdpi': '../../../src-nacoes/Android/res/drawable-hdpi/icon.png',
+	'android_xhdpi': '../../../src-nacoes/Android/res/drawable-xhdpi/icon.png',
+	'android_xxhdpi': '../../../src-nacoes/Android/res/drawable-xxhdpi/icon.png',
+	'android_xxxhdpi': '../../../src-nacoes/Android/res/drawable-xxxhdpi/icon.png'
 });
 App.launchScreens({
-	'android_mdpi_portrait': '../../src-techno/Android/res/drawable-mdpi/screen.png',
-	'android_hdpi_portrait': '../../src-techno/Android/res/drawable-hdpi/screen.png',
-	'android_xhdpi_portrait': '../../src-techno/Android/res/drawable-xhdpi/screen.png',
-	'android_xxhdpi_portrait': '../../src-techno/Android/res/drawable-xxhdpi/screen.png'
+	'android_mdpi_portrait': '../../../src-nacoes/Android/res/drawable-mdpi/screen.png',
+	'android_hdpi_portrait': '../../../src-nacoes/Android/res/drawable-hdpi/screen.png',
+	'android_xhdpi_portrait': '../../../src-nacoes/Android/res/drawable-xhdpi/screen.png',
+	'android_xxhdpi_portrait': '../../../src-nacoes/Android/res/drawable-xxhdpi/screen.png'
 });
 App.setPreference('android-minSdkVersion', '16');
 App.setPreference('BackgroundColor', '0xffffffff');
@@ -29,7 +29,7 @@ App.setPreference('StatusBarOverlaysWebView', 'true');
 App.setPreference('StatusBarBackgroundColor', 'transparent');
 
 App.configurePlugin('phonegap-plugin-push', {
-	SENDER_ID: 862875202989
+	SENDER_ID: 1000788241076
 });
 
 // Acesso as imagens GridFS
@@ -37,6 +37,7 @@ App.accessRule('*', true)
 App.accessRule('blob:*')
 App.accessRule('data:*', { type: 'navigation' })
 // Permite acesso a alguns sites importantes e uteis
+App.accessRule('https://res.cloudinary.com/*');
 App.accessRule('https://panel.applegal.com.br/*');
 App.accessRule('https://m.facebook.com/*');
 App.accessRule('https://facebook.com/*');
